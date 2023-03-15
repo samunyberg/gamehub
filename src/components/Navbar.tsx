f9e2fc9761702f2cb34476b66f6bd2029abf84ad
Samu Nyberg <samu.nyberg@gmail.com>
2023-03-15 15:36:55 +0200

    Modify navbar padding.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Navbar = ({ onSearch }: Props) => {
   return (
-    <HStack padding="10px">
+    <HStack paddingY="10px" paddingX="20px">
       <Image src={logo} boxSize="60px" />
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
